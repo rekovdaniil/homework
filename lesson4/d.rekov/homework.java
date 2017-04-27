2b7e249661924ea2423970bacf2dbc6ab2eeb54c
Daniil Rekov <rekov.daniil@gmail.com>
2017-04-27 02:30:45 -0700

    TASK 1 VARIABLES

---
+++ lesson4/d.rekov/homework.java
@@ -0,0 +1,17 @@
+
+public class homework
+{
+  public static void main(String[] args)
+  {
+  // Task 1
+  System.out.printf("______________________\n");
+  System.out.printf("TASK 1 VARIABLES\n");
+  System.out.printf("______________________\n");
+    int a = 1000;
+    float b = 14.88f;
+    String c = "Task 1 String Variable";
+    System.out.printf("Integer = %d \nFloat = %.2f \nString = %s\n",a,b,c);
+    System.out.printf("______________________\n");
+
+  }
+}
